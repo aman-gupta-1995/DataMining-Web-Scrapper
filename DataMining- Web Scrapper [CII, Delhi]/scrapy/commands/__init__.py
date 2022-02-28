5f778b868628672c8daa947f8df7a49e8c94b3dd
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-01 00:43:15 +0530

    Update __init__.py

--- DataMining- Web Scrapper [CII, Delhi]/scrapy/commands/__init__.py
+++ DataMining- Web Scrapper [CII, Delhi]/scrapy/commands/__init__.py
@@ -5,7 +5,7 @@ import os
 from optparse import OptionGroup
 from twisted.python import failure
   
-from scrapy.utils.conf import arglist_to_dict
+from scrapy.utils.conf import arglist_to_dict 
 from scrapy.exceptions import UsageError  
  
 
