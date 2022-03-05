469aa686de0862b167b51d989d3bfb1b612880d6
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-05 14:46:22 +0530

    Update conftest.py

--- DataMining- Web Scrapper [CII, Delhi]/docs/conftest.py
+++ DataMining- Web Scrapper [CII, Delhi]/docs/conftest.py
@@ -1,6 +1,6 @@
 import os
 from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
-
+ 
 from scrapy.http.response.html import HtmlResponse 
 from sybil import Sybil 
 from sybil.parsers.codeblock import CodeBlockParser 
